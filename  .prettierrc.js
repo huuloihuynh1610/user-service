5b3a426cc618ea2a31fa10b7ca1ed507b663fe54
nluit <nluit99@gmail.com>
2021-03-07 17:38:56 +0700

    init project

---
+++  .prettierrc.js
@@ -0,0 +1,11 @@
+{
+    "jsxSingleQuote": true,
+    "useTabs": false,
+    "tabWidth": 2,
+    "jsonEnable": [],
+    "openOutput": false,
+    "semi": false,
+    "singleQuote": true,
+    "printWidth": 120
+  }
+  
